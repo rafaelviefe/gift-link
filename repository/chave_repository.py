f8672b76d7784342c4de483df843a79fe22b2007
rafaelviefe <rafaelviefe@gmail.com>
2025-09-10 00:35:34 -0300

    Add chave_repository.py for key validation functionality

---
+++ repository/chave_repository.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+from typing import Tuple
+from model.chave import Chave
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+def verificar_chave(chave_para_verificar: Chave) -> Tuple[bool, str]:
+
+    try:
+        chave_id = chave_para_verificar.get_id()
+        response = supabase.table('chave_acesso').select('id').eq('id', chave_id).execute()
+
+        return (bool(response.data), "Chave de acesso válida." if response.data else "Chave de acesso inválida.")
+
+    except Exception as e:
+        return (False, f"Ocorreu um erro inesperado no servidor: {e}")
