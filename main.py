d7b9a37e25289c90f5d8b161b98ff460caea7ad8
rafaelviefe <rafaelviefe@gmail.com>
2025-09-13 17:08:39 -0300

    Refactor and reorganize view files for improved structure and clarity

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import FreeSimpleGUI as sg
 from views import (
-    tela_inicial_view,
-    tela_cadastro_view,
-    tela_login_view,
-    tela_organizador_view,
-    tela_participante_view,
-    tela_cadastro_participante_view,
-    tela_alterar_senha_view
+    tela_alterar_senha,
+    tela_cadastro,
+    tela_cadastro_participante,
+    tela_inicial,
+    tela_login,
+    tela_organizador,
+    tela_participante
 )
 from controller import organizador_controller, participante_controller
 from model.organizador import Organizador
@@ -18,7 +18,7 @@ def main():
     participante_logado: Optional[Participante] = None
     participante_pendente: Optional[Participante] = None
 
-    janela_inicial = tela_inicial_view.criar_janela_inicial()
+    janela_inicial = tela_inicial.criar_janela_inicial()
     janela_cadastro, janela_login, janela_organizador, janela_participante, janela_cadastro_participante, janela_alterar_senha = None, None, None, None, None, None
 
     while True:
@@ -30,10 +30,10 @@ def main():
         if janela_ativa == janela_inicial:
             if evento == "-CADASTRO-":
                 janela_inicial.hide()
-                janela_cadastro = tela_cadastro_view.criar_janela_cadastro()
+                janela_cadastro = tela_cadastro.criar_janela_cadastro()
             elif evento == "-LOGIN-":
                 janela_inicial.hide()
-                janela_login = tela_login_view.criar_janela_login()
+                janela_login = tela_login.criar_janela_login()
 
         elif janela_ativa == janela_cadastro:
             if evento == "-VOLTAR-":
@@ -55,7 +55,7 @@ def main():
                     janela_cadastro.close()
                     janela_cadastro = None
                     janela_inicial.close()
-                    janela_organizador = tela_organizador_view.criar_janela_organizador(
+                    janela_organizador = tela_organizador.criar_janela_organizador(
                         organizador_logado
                     )
                 else:
@@ -78,7 +78,7 @@ def main():
                         janela_login.close()
                         janela_login = None
                         janela_inicial.close()
-                        janela_organizador = tela_organizador_view.criar_janela_organizador(organizador_logado)
+                        janela_organizador = tela_organizador.criar_janela_organizador(organizador_logado)
                     else:
                         sg.popup_error(mensagem)
                 else:
@@ -87,7 +87,7 @@ def main():
                         if not participante.is_elegivel():
                             sg.popup("Você precisa alterar sua senha para continuar.")
                             participante_pendente = participante
-                            janela_alterar_senha = tela_alterar_senha_view.criar_janela_alterar_senha(participante.get_username())
+                            janela_alterar_senha = tela_alterar_senha.criar_janela_alterar_senha(participante.get_username())
                             janela_login.hide()
                         else:
                             sg.popup_ok(mensagem)
@@ -95,7 +95,7 @@ def main():
                             janela_login.close()
                             janela_login = None
                             janela_inicial.close()
-                            janela_participante = tela_participante_view.criar_janela_participante(participante_logado)
+                            janela_participante = tela_participante.criar_janela_participante(participante_logado)
                     else:
                         sg.popup_error(mensagem)
 
@@ -121,7 +121,7 @@ def main():
                         janela_login = None
                         janela_inicial.close()
                         
-                        janela_participante = tela_participante_view.criar_janela_participante(participante_logado)
+                        janela_participante = tela_participante.criar_janela_participante(participante_logado)
                     else:
                         sg.popup_error(mensagem)
 
@@ -130,12 +130,12 @@ def main():
                 organizador_logado = None
                 janela_organizador.close()
                 janela_organizador = None
-                janela_inicial = tela_inicial_view.criar_janela_inicial()
+                janela_inicial = tela_inicial.criar_janela_inicial()
             elif evento == "-PARTICIPANTES-":
                 janela_organizador.hide()
                 participantes, _msg = participante_controller.listar_participantes()
                 janela_cadastro_participante = (
-                    tela_cadastro_participante_view.criar_janela_cadastro(participantes)
+                    tela_cadastro_participante.criar_janela_cadastro(participantes)
                 )
             elif evento == "-EVENTOS-":
                 sg.popup(
@@ -147,7 +147,7 @@ def main():
                 participante_logado = None
                 janela_participante.close()
                 janela_participante = None
-                janela_inicial = tela_inicial_view.criar_janela_inicial()
+                janela_inicial = tela_inicial.criar_janela_inicial()
             elif evento in ["-MEUSEVENTOS-", "-DESEJOS-"]:
                 sg.popup(f"Funcionalidade de '{evento.strip('-')}' será implementada em breve!")
 
@@ -163,7 +163,7 @@ def main():
                 )
                 sg.popup(mensagem)
                 participantes, _msg = participante_controller.listar_participantes()
-                tela_cadastro_participante_view.atualizar_tabela(
+                tela_cadastro_participante.atualizar_tabela(
                     janela_cadastro_participante, participantes
                 )
                 try:
