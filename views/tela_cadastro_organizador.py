d144fc163a4bb0569ee2fd7c2728b2d5a0ef26aa
rafaelviefe <rafaelviefe@gmail.com>
2025-10-12 11:36:56 -0300

    Fix label typo in organizer registration window from 'Código de Acesso' to 'Chave de Acesso'

--- views/tela_cadastro_organizador.py
+++ views/tela_cadastro_organizador.py
@@ -13,7 +13,7 @@ class TelaCadastroOrganizador:
             [sg.Text('Cadastro de Organizador', font=('Helvetica', 20, 'bold'))],
             [sg.Text('Username', size=(15, 1)), sg.InputText(key='-USERNAME-')],
             [sg.Text('Senha', size=(15, 1)), sg.InputText(key='-SENHA-', password_char='*')],
-            [sg.Text('Código de Acesso', size=(15, 1)), sg.InputText(key='-CHAVE-')],
+            [sg.Text('Chave de Acesso', size=(15, 1)), sg.InputText(key='-CHAVE-')],
             [sg.Button('Cadastrar', key='-SUBMIT-'), sg.Button('Voltar', key='-VOLTAR-')]
         ]
         return sg.Window(
